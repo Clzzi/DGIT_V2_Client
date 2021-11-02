6404d2e01652ff8ad6214d9b85e7ed9317f72e99
Clzzi <clzzi1109@naver.com>
2021-11-02 14:23:42 +0900

    :heavy_plus_sign: Add: Recoil Lib
    Recoil 라이브러리 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
 import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from 'styles/GlobalStyle';
+import { lightTheme } from 'styles/ThemeConfig';
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={lightTheme}>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 };
 
 export default App;
