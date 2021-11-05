6b90c5ade4107efda45a7f55ef6f22c4efc0ffcd
Clzzi <clzzi1109@naver.com>
2021-11-05 10:20:28 +0900

    Fix Banner Position

--- components/Banner/index.tsx
+++ components/Banner/index.tsx
@@ -1,11 +1,18 @@
 import Image from 'next/image';
-import BannerImage from 'assets/Banner.png';
+import BannerImg from 'public/static/Banner.png';
 import * as S from './styles';
 
 const Banner = (): JSX.Element => {
+  const prefix =
+    process.env.NODE_ENV === 'production' ? 'http://dgit.b1nd.com' : '';
   return (
     <S.BannerWrap>
-      <Image src={BannerImage.src} alt="Banner" width={1410} height={170} />
+      <Image
+        src={`${prefix}${BannerImg.src}`}
+        alt="Banner"
+        width={1410}
+        height={170}
+      />
     </S.BannerWrap>
   );
 };
