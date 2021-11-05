74eb19cf3746b436c46a19c1a901723f64820cdc
Clzzi <clzzi1109@naver.com>
2021-11-05 10:55:16 +0900

    Fix: Banner Image png -> jpg

--- components/Banner/index.tsx
+++ components/Banner/index.tsx
@@ -1,18 +1,11 @@
 import Image from 'next/image';
-import BannerImg from 'public/static/Banner.png';
 import * as S from './styles';
+import BannerImg from 'public/static/Banner.jpg';
 
 const Banner = (): JSX.Element => {
-  const prefix =
-    process.env.NODE_ENV === 'production' ? 'http://dgit.b1nd.com' : '';
   return (
     <S.BannerWrap>
-      <Image
-        src={`${prefix}${BannerImg.src}`}
-        alt="Banner"
-        width={1410}
-        height={170}
-      />
+      <Image src={BannerImg} alt="Banner" width={1410} height={170} />
     </S.BannerWrap>
   );
 };
