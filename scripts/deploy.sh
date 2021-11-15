bcdcfd3f87d2a86403613fef5af3ddf4c9aed63b
Clzzi <clzzi1109@naver.com>
2021-11-15 21:54:59 +0900

    :pencil2: fix
    ci

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,9 @@
-cd
-cd DGIT_V2_Client
-
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+nvm install 14.18.0
+nvm use 14.18.0
+npm i -g yarn
+npm i -g pm2
+yarn
+yarn build
 pm2 delete all
 pm2 start yarn -w -i 0 --name "DGIT-V2" -- start
