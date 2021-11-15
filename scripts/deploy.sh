08ff0542cd74cdfc57d7f36810706b4b9928db21
Clzzi <clzzi1109@naver.com>
2021-11-15 22:26:25 +0900

    :pencil2: Fix
    CI

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,4 @@
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
-nvm install 14.18.0
-nvm use 14.18.0
-npm i -g yarn
-npm i -g pm2
-yarn
-yarn build
-pm2 delete all
-pm2 start yarn -w -i 0 --name "DGIT-V2" -- start
+#!/bin/bash
+npm install pm2 -g
+pm2 stop admin || true 
+pm2 delete admin || true 
