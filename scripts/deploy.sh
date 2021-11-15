1dfa696b4699cfc6e1926bf9f7229641b657dc43
Clzzi <clzzi1109@naver.com>
2021-11-15 22:46:00 +0900

    :green_heart: CI
    Fix

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,10 +3,6 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 export HOME="/home/ubuntu/"
-sudo PM2_HOME=/home/ubuntu/.pm2 pm2 list
-cd /home/ubuntu/DGIT_V2_Client
-git pull
-npm install
-npm run build
+npm install -g pm2
 pm2 delete all
 pm2 start yarn -w -i 0 --name "DGIT-V2" -- start
