aecd8511573ed0dbca9c8a8dd9a57f87a0965522
Clzzi <clzzi1109@naver.com>
2021-11-16 12:54:26 +0900

    Fix: CI Command

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-source /home/ubuntu/
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-export HOME="/home/ubuntu/"
 npm install -g pm2
 pm2 delete all
 pm2 start yarn -w -i 0 --name "DGIT-V2" -- start
