ba9d42b602be962a5e5eeb1a26db1352f6b02b21
Clzzi <clzzi1109@naver.com>
2021-11-16 20:39:45 +0900

    Fix: CI

--- scripts/before_deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd /home/ubuntu
-
-pm2 delete all
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
+PATH="/home/ubuntu/.local/bin:$PATH"
 
 cd  /home/ubuntu/build
 
-ls 
-ll
-
+pm2 delete all
 pm2 start yarn -w -i 0 --name "DGIT-V2" -- start
