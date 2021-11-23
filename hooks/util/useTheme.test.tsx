91738c0254ff21a1b32fa77d360a002e96d96a2f
Clzzi <clzzi1109@naver.com>
2021-11-23 13:02:55 +0900

    :white_check_mark: Add: useTheme Testing window.matchMedia
    window.matchMedia method로 테마 테스트코드 작성

--- hooks/util/useTheme.test.tsx
+++ hooks/util/useTheme.test.tsx
@@ -2,15 +2,34 @@ import useTheme from './useTheme';
 import { act } from '@testing-library/react-hooks';
 import { darkTheme, lightTheme } from 'styles/ThemeConfig';
 import { customRenderHook } from 'util/customRenderHook';
+import local from 'util/local';
+import { THEME_KEY } from 'constants/theme.constants';
+import { ThemeEnum } from 'enum/ThemeEnum';
+import '__mocks__/matchMedia';
 
 describe('TEST useTheme', () => {
-  it('return Value Correctly on LightMode', () => {
+  it('return Value Correctly on LightMode(system)', () => {
     const { result } = customRenderHook(() => useTheme());
     expect(result.current.theme).toEqual(lightTheme);
     expect(result.current.currentTheme).toEqual(0);
   });
 
-  it('return Value Correctly on DarkMode', () => {
+  it('return Value Correctly on DarkMode(system)', () => {
+    const { result } = customRenderHook(() => useTheme());
+    act(() => result.current.handleChangeTheme());
+    expect(result.current.theme).toEqual(darkTheme);
+    expect(result.current.currentTheme).toEqual(1);
+  });
+
+  it('return Value Correctly on LightMode(localStorage)', () => {
+    local.set(THEME_KEY, ThemeEnum.LIGHT.toString());
+    const { result } = customRenderHook(() => useTheme());
+    expect(result.current.theme).toEqual(lightTheme);
+    expect(result.current.currentTheme).toEqual(0);
+  });
+
+  it('return Value Correctly on DarkMode(localStorage)', () => {
+    local.set(THEME_KEY, ThemeEnum.DARK.toString());
     const { result } = customRenderHook(() => useTheme());
     act(() => result.current.handleChangeTheme());
     expect(result.current.theme).toEqual(darkTheme);
--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  setupFilesAfterEnv: [
+    '<rootDir>/jest.setup.ts',
+    '<rootDir>/__mocks__/matchMedia.ts',
+  ],
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
   },
