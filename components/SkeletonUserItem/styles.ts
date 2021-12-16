fcf08dd86f42834f7ed8e751797ae27275987911
Clzzi <clzzi1109@naver.com>
2021-12-16 12:26:53 +0900

    :lipstick: Fix: Skeleton UI Media Query
    스켈레톤 컴포넌트 미디어 쿼리 적용

--- components/SkeletonUserItem/styles.ts
+++ components/SkeletonUserItem/styles.ts
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
 import * as M from 'styles/moduleStyle';
 export const UserItem = styled.div`
-  width: 1410px;
+  max-width: 1410px;
+  width: 100%;
   height: 130px;
   display: flex;
   flex-direction: row;
@@ -10,6 +11,10 @@ export const UserItem = styled.div`
   text-align: center;
   background-color: ${({ theme }) => theme.skeletonBackground};
   margin-bottom: 4px;
+  @media screen and (max-width: 767px) {
+    padding: 0px 25px;
+    height: 80px;
+  }
 `;
 
 export const UserItemProfile = styled.div`
@@ -19,12 +24,18 @@ export const UserItemProfile = styled.div`
   background-color: ${({ theme }) => theme.skeletonContent};
   margin: 0px 25px 0px 25px;
   animation: ${M.skeletonAnimation} 1.5s infinite ease-in-out;
+  @media screen and (max-width: 767px) {
+    width: 48px;
+    height: 48px;
+    margin: 5px;
+  }
 `;
 
 export const UserItemContent = styled.div<{
   width: string;
   height: string;
   margin?: string;
+  padding?: string;
 }>`
   width: ${(props) => props.width};
   height: ${(props) => props.height};
@@ -32,4 +43,24 @@ export const UserItemContent = styled.div<{
   background-color: ${({ theme }) => theme.skeletonContent};
   margin: ${(props) => props.margin};
   animation: ${M.skeletonAnimation} 1.5s infinite ease-in-out;
+  padding: ${(props) => props.padding};
+  @media screen and (max-width: 767px) {
+    &:nth-child(1) {
+      width: 20px;
+      margin: 0px 4px 0px 4px;
+    }
+    &:nth-child(2) {
+      display: none;
+    }
+    &:nth-child(3) {
+      display: none;
+    }
+    &:nth-child(4) {
+      display: none;
+    }
+    &:nth-child(5) {
+      width: 200px;
+      margin: 0px 4px 0px 4px;
+    }
+  }
 `;
